19bbab628258cf432d586c68b71881a244fbd8fd
itsurkunal <88033499+itsurkunal@users.noreply.github.com>
2023-10-07 06:10:31 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,106 @@
+# Password Generator Project
+import random
+
+letters = [
+  'a','b','c', 'd',
+  'e',
+  'f',
+  'g',
+  'h',
+  'i',
+  'j',
+  'k',
+  'l',
+  'm',
+  'n',
+  'o',
+  'p',
+  'q',
+  'r',
+  's',
+  't',
+  'u',
+  'v',
+  'w',
+  'x',
+  'y',
+  'z',
+  'A',
+  'B',
+  'C',
+  'D',
+  'E',
+  'F',
+  'G',
+  'H',
+  'I',
+  'J',
+  'K',
+  'L',
+  'M',
+  'N',
+  'O',
+  'P',
+  'Q',
+  'R',
+  'S',
+  'T',
+  'U',
+  'V',
+  'W',
+  'X',
+  'Y',
+  'Z',]
+numbers = ['1', '2', '3', '4', '5', '6', '7', '8', '9']
+symbols = ['!', '#', '$', '%', '&', '(', ')', '*', '+']
+
+print("Welcome to the Py Password Generator!")
+nr_letters = int(input("How many letters would you like in your password?\n"))
+nr_symbols = int(input("How many symbols would you like?\n"))
+nr_numbers = int(input("How many numbers would you like?\n"))
+
+#Easy Level
+# fgkh^&23
+
+#Hard Level
+# g^2jk8&
+
+# # Eazy Level
+# password = ""
+# # nr_letters = 4
+# for char in range(1, nr_letters+1):
+#   #1 - 4
+#   # random_char = random.choice(letters)
+#   # password += random_char
+#   # print(password)
+#   password += random.choice(letters)
+
+# for char in range(1, nr_symbols+1):
+#   password += random.choice(symbols)
+
+# for char in range(1, nr_numbers+1):
+#   password += random.choice(numbers)
+
+# print(password)
+
+# Hard level
+password_list = []
+
+for char in range(1, nr_letters + 1):
+  password_list.append(random.choice(letters))
+
+for char in range(1, nr_symbols + 1):
+  password_list.append(random.choice(symbols))
+
+for char in range(1, nr_numbers + 1):
+  password_list.append(random.choice(numbers))
+
+print(password_list)
+random.shuffle(password_list)
+print(password_list)
+
+password = ""
+for char in password_list:
+  password += char
+
+print(f"Your Password is {password}")
